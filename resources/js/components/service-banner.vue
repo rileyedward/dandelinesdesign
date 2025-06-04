a7e4ce49ba7456b26b4661baf435b62a538a9d15
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-03 22:13:26 -0500

    sizing

--- resources/js/components/service-banner.vue
+++ resources/js/components/service-banner.vue
@@ -6,13 +6,13 @@ defineProps<Props>();
 
 <template>
     <div class="flex flex-col py-6 md:flex-row" :class="{ 'md:flex-row-reverse': !service.imageOnLeft }">
-        <div class="w-full bg-white p-4 md:w-1/3">
+        <div class="w-full bg-white p-4 md:w-2/5">
             <div class="h-90 border-2 border-black p-2">
                 <img :src="service.image_url" alt="Service Image" class="h-full w-full object-cover object-center" />
             </div>
         </div>
 
-        <div class="flex w-full flex-col justify-center md:w-2/3" :class="{ 'text-left': service.imageOnLeft, 'text-right': !service.imageOnLeft }">
+        <div class="flex w-full flex-col justify-center md:w-3/5" :class="{ 'text-left': service.imageOnLeft, 'text-right': !service.imageOnLeft }">
             <div class="px-8 pt-4">
                 <h2 class="mb-1 text-xl font-bold">{{ service.title }}</h2>
             </div>
