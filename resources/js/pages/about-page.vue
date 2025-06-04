16305bc4f4d837826eede122687adf9feaa8aed1
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-03 23:48:06 -0500

    about-widget

--- resources/js/pages/about-page.vue
+++ resources/js/pages/about-page.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import AboutSection from '@/components/about-section.vue';
+import AboutWidget from '@/components/about-widget.vue';
 import Background from '@/components/background/background.vue';
 import TestimonialsSection from '@/components/testimonials-section.vue';
 import AppLayout from '@/layouts/app-layout.vue';
@@ -11,6 +12,7 @@ import AppLayout from '@/layouts/app-layout.vue';
             <div class="flex h-full items-center justify-center">
                 <h1 class="text-4xl font-bold text-white">About Us</h1>
             </div>
+            <about-widget />
         </background>
 
         <div class="container mx-auto px-4 py-12">
