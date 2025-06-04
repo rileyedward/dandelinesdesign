257ffe71c33f2f93c63b2a66fd57a7628b03b98a
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-03 19:21:49 -0500

    cleanup codebase

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,9 +5,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
-import { initializeTheme } from './composables/useAppearance';
 
-// Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
     interface ImportMetaEnv {
         readonly VITE_APP_NAME: string;
@@ -20,7 +18,7 @@ declare module 'vite/client' {
     }
 }
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Dandelines Design';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -34,7 +32,4 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
-});
-
-// This will set light / dark mode on page load...
-initializeTheme();
+}).then(() => console.log('Inertia app loaded...'));
---
+++ resources/js/layouts/app-layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { AppLayoutProps as Props } from '@/types/layouts/app-layout';
+import { Head as InertiaHead } from '@inertiajs/vue3';
+
+withDefaults(defineProps<Props>(), {
+    pageTitle: 'Dandelines Design',
+});
+</script>
+
+<template>
+    <inertia-head :title="pageTitle" />
+
+    <slot />
+</template>
