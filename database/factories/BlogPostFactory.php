ceaf2f0a8cc7698be96c65ca881958ba9e18ff27
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-06 11:12:27 -0500

    random images

--- database/factories/BlogPostFactory.php
+++ database/factories/BlogPostFactory.php
@@ -8,12 +8,17 @@ class BlogPostFactory extends Factory
 {
     public function definition(): array
     {
+        $imagesPath = public_path('images');
+        $images = array_filter(scandir($imagesPath), function ($item) use ($imagesPath) {
+            return ! is_dir($imagesPath.'/'.$item) && $item !== '.' && $item !== '..' && $item !== '.DS_Store';
+        });
+
         return [
             'title' => fake()->sentence(6, true),
             'slug' => str()->slug(fake()->sentence(6, true)),
             'content' => fake()->realText(3000, 2),
             'excerpt' => fake()->realText(200),
-            'image_url' => fake()->imageUrl(),
+            'image_url' => '/images/'.($images ? $images[array_rand($images)] : ''),
             'is_published' => fake()->boolean(),
         ];
     }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,10 +8,15 @@ class ProductFactory extends Factory
 {
     public function definition(): array
     {
+        $imagesPath = public_path('images');
+        $images = array_filter(scandir($imagesPath), function ($item) use ($imagesPath) {
+            return ! is_dir($imagesPath.'/'.$item) && $item !== '.' && $item !== '..' && $item !== '.DS_Store';
+        });
+
         return [
             'name' => fake()->words(3, true),
             'description' => fake()->paragraph(),
-            'image_url' => fake()->imageUrl(),
+            'image_url' => '/images/'.($images ? $images[array_rand($images)] : ''),
             'price' => fake()->randomFloat(2, 5, 100),
             'is_available' => fake()->boolean(80),
         ];
