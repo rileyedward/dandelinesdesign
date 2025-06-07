d81f5bbd3a795a0b0921101a217f164169edd848
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-07 11:09:44 -0500

    scaffold new pages

---
+++ resources/js/pages/admin/admin-contact.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AdminLayout from '@/layouts/admin-layout.vue';
+import { FileText, MessageSquare, ShoppingBag, Quote, Star } from 'lucide-vue-next';
+</script>
+
+<template>
+    <admin-layout page-title="Admin Dashboard">
+        <h1>Hi</h1>
+    </admin-layout>
+</template>
