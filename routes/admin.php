8e7d6cf4614c280450a5ed428e2c487bc55bff05
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-07 10:45:17 -0500

    admin page

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::middleware(['auth'])
     ->name('admin.')
     ->prefix('/admin')
     ->group(function () {
-        //
+        Route::get('/', [AdminController::class, 'index'])->name('index');
     });
