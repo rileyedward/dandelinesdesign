2bf767cc138fb1d079d93cc87a5e72cb961b8d23
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-07 10:48:40 -0500

    admin layout

---
+++ resources/js/layouts/admin-layout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { AdminLayoutProps as Props } from '@/types/layouts/admin-layout';
+import { Head as InertiaHead } from '@inertiajs/vue3';
+import Footer from '@/components/footer/footer.vue';
+
+withDefaults(defineProps<Props>(), {
+    pageTitle: 'Dandelines Admin',
+});
+</script>
+
+<template>
+    <inertia-head :title="pageTitle" />
+
+    <main>
+        <slot />
+    </main>
+
+    <Footer />
+</template>
---
+++ resources/js/pages/admin/admin-index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AdminLayout from '@/layouts/admin-layout.vue';
+</script>
+
+<template>
+    <admin-layout>
+        <h1>Admin</h1>
+    </admin-layout>
+</template>
