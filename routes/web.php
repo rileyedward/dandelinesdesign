6a0a24a32a257e6669eb6005d4cd10e72dcf6302
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-06-07 11:03:50 -0500

    cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\ConstructionController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\StoreController;
-use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
 Route::get('/store', [StoreController::class, 'index'])->name('store.index');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/quote', [QuoteController::class, 'store'])->name('quote.store');
 
 Route::controller(BlogController::class)
     ->prefix('/blog')
@@ -24,30 +24,6 @@ Route::controller(BlogController::class)
     ->group(function () {
         Route::get('/', 'index')->name('blog.index');
         Route::get('/{blog_post:slug}', 'show')->name('blog.show');
-    });
-
-Route::controller(ContactController::class)
-    ->prefix('/contact')
-    ->name('contact.')
-    ->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-    });
-
-Route::post('/quote', [QuoteController::class, 'store'])->name('quote.store');
-
-Route::controller(ProductController::class)
-    ->prefix('/products')
-    ->name('products.')
-    ->group(function () {
-        Route::post('/', 'store')->name('store');
-    });
-
-Route::controller(TestimonialController::class)
-    ->prefix('/testimonials')
-    ->name('testimonials.')
-    ->group(function () {
-        Route::post('/', 'store')->name('store');
     });
 
 Route::controller(ConstructionController::class)
