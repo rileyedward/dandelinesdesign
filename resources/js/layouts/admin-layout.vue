c5a78ef8762e50befaba4726c6969dae3c958982
Riley Grotenhuis <rileygrotenhuis@Mac.localdomain>
2025-06-11 11:43:03 -0500

    admin-layout cleanup

--- resources/js/layouts/admin-layout.vue
+++ resources/js/layouts/admin-layout.vue
@@ -17,7 +17,6 @@ withDefaults(defineProps<Props>(), {
     <div class="min-h-screen bg-gray-50">
         <div class="md:ml-64">
             <main class="container mx-auto px-4 py-12">
-                <!-- Page Header -->
                 <div class="mb-8">
                     <h1 class="text-3xl font-bold text-gray-900">{{ pageTitle }}</h1>
                     <p v-if="pageDescription" class="mt-2 text-lg text-gray-600">{{ pageDescription }}</p>
