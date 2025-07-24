9b76ef64f982f710b73a5ee3cbb27a129b9b9f14
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-07-24 18:00:17 -0500

    format code

--- resources/js/components/services/services-widget.vue
+++ resources/js/components/services/services-widget.vue
@@ -7,9 +7,8 @@
         <hr class="mx-auto mb-3 w-16 border-t border-gray-300" />
 
         <p class="text-center text-sm text-gray-700">
-            From stunning floral arrangements to comprehensive event planning and creative design, we offer a range of
-            services tailored to bring your vision to life. Each service is crafted with attention to detail and a
-            commitment personal and excellent service.
+            From stunning floral arrangements to comprehensive event planning and creative design, we offer a range of services tailored to bring your
+            vision to life. Each service is crafted with attention to detail and a commitment personal and excellent service.
         </p>
     </div>
 </template>
