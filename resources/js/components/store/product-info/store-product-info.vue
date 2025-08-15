6d8be38a36b9ef9ed9006371994a138dc9c2b257
Riley Grotenhuis <rileygrotenhuis@Mac.attlocal.net>
2025-08-15 18:24:41 -0500

    product -info

--- resources/js/components/store/product-info/store-product-info.vue
+++ resources/js/components/store/product-info/store-product-info.vue
@@ -118,13 +118,6 @@ const handleAddToCart = () => {
                                 Digital Product
                             </span>
                             <span
-                                v-if="product.shippable"
-                                class="inline-flex items-center rounded-full bg-green-100 px-3 py-1 text-sm font-medium text-green-800"
-                            >
-                                <TruckIcon class="mr-1 h-4 w-4" />
-                                Free Shipping
-                            </span>
-                            <span
                                 v-if="isOutOfStock"
                                 class="inline-flex items-center rounded-full bg-red-100 px-3 py-1 text-sm font-medium text-red-800"
                             >
